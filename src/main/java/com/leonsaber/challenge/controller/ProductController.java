42bc8571510e23995474830415f1b1c1a8eb77df
Leon <168288646@qq.com>
2018-04-09 13:17:02 -0700

    cycle check

--- src/main/java/com/leonsaber/challenge/controller/ProductController.java
+++ src/main/java/com/leonsaber/challenge/controller/ProductController.java
@@ -4,54 +4,69 @@ package com.leonsaber.challenge.controller;
 import com.leonsaber.challenge.model.entity.Product;
 import com.leonsaber.challenge.model.service.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@Controller
+@RestController
+@RequestMapping(value = "/v1/product")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
-
-    @GetMapping(path = "/product/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public @ResponseBody
     Product findOutOfStack(@PathVariable("id")int productID) {
+
         return productRepository.findProductByProductID(productID);
     }
 
-    @GetMapping(path = "/product/all")
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public @ResponseBody
     List<Product> findAll() {
         return productRepository.findAllByOrderByProductIDAsc();
     }
 
-    @GetMapping(path = "/product/top-10")
+    @RequestMapping(value = "/top10", method = RequestMethod.GET)
     public @ResponseBody
     List<Product> findTop10() {
         return productRepository.findTop10ByOrderByProductQtyAsc();
     }
 
-    @GetMapping(path = "/product/out-of-stack")
+    @RequestMapping(value = "/outOfStock", method = RequestMethod.GET)
     public @ResponseBody
     List<Product> findOutOfStack() {
         return productRepository.findOutOfStack();
     }
 
-    @GetMapping(path = "/product/price-asc")
+    @RequestMapping(value = "/priceAsc", method = RequestMethod.GET)
     public @ResponseBody
     List<Product> findPriceAsc() {
         return productRepository.findAllByOrderByProductPriceAsc();
     }
 
-    @GetMapping(path = "/product/price-desc")
+    @RequestMapping(value = "/priceDesc", method = RequestMethod.GET)
     public @ResponseBody
     List<Product> findPriceDesc() {
         return productRepository.findAllByOrderByProductPriceDesc();
+    }
+
+    @RequestMapping(value = "/priceRecommend={price}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Product> findPriceRecommend(@PathVariable("price") double price) {
+        return productRepository.findProductsByProductPrice(price);
+    }
+
+    @RequestMapping(value = "/lastOne", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Product> findPriceRecommend() {
+        return productRepository.findProductsByProductQtyEqualsOne();
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public @ResponseBody
+    void deleteProduct(@PathVariable("id")int productID) {
+        productRepository.deleteById(productID);
     }
 }
--- src/main/java/com/leonsaber/challenge/model/service/ProductRepository.java
+++ src/main/java/com/leonsaber/challenge/model/service/ProductRepository.java
@@ -2,6 +2,7 @@ package com.leonsaber.challenge.model.service;
 
 import com.leonsaber.challenge.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 
@@ -12,8 +13,11 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findAllByOrderByProductIDAsc();
 
+    @Modifying
+    @Query(nativeQuery=true, value = "SELECT * FROM (SELECT * FROM product WHERE product_qty > 0) p ORDER BY p.product_qty ASC LIMIT 10; ")
     List<Product> findTop10ByOrderByProductQtyAsc();
 
+    @Modifying
     @Query(nativeQuery=true, value = "SELECT * FROM product p WHERE p.product_qty = 0")
     List<Product> findOutOfStack();
 
@@ -22,4 +26,12 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAllByOrderByProductPriceDesc();
 
     Product findProductByProductID(int productID);
+
+    @Query(nativeQuery=true, value = "SELECT * FROM product p WHERE p.product_price >= abs(?1 - 0.5) AND p.product_price <= ?1 + 0.5")
+    List<Product> findProductsByProductPrice(double productPrice);
+
+    @Query(nativeQuery=true, value = "SELECT * FROM product p WHERE p.product_qty = 1")
+    List<Product> findProductsByProductQtyEqualsOne();
+
+    void deleteById(int productID);
 }
